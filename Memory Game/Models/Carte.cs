8b683ec9c7d66a2aeecfe4856364a535c8de18b6
dalexandra921 <alexandra-elena.dumitrescu@student.upt.ro>
2026-05-19 08:19:42 +0300

    Intro page

---
+++ Memory Game/Models/Carte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_Game.Models
+{
+    public class Carte
+    {
+        public int Id { get; set; }
+        public string Simbol { get; set; }
+        public string Imagine { get; set; }
+        public bool EsteIntoarsa { get; set; }
+        public bool EsteGasita { get; set; }
+    }
+}
---
+++ Memory Game/Models/Joc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_Game.Models
+{
+    public class Joc
+    {
+        public List<Carte> ListaCarti { get; set; }
+        public int Scor { get; set; }
+        public int Mutari { get; set; }
+        public int TimpJoc { get; set; }
+    }
+}
